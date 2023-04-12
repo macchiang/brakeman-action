b62dcb780931bf918a69e4346c30315c7c6b5c30
Mac Chiang <macchiang@gmail.com>
2023-04-12 12:20:34 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ export TARGET_PATH=${INPUT_PATH:-.}
 export OPTIONS=${INPUT_OPTIONS:---no-progress --no-exit-on-warn --no-exit-on-error --color -o /dev/stdout -o brakeman_results.html}
 
 git config --global --add safe.directory ${GITHUB_WORKSPACE}
-COMMAND="bundle exec brakeman -p ${TARGET_PATH} ${OPTIONS}"
+COMMAND="bundle exec brakeman -p ${GITHUB_WORKSPACE}/${TARGET_PATH} ${OPTIONS}"
 echo "COMMAND: ${COMMAND}"
 $COMMAND
